951dfdba740b1e8bc816305e8cddd9508110d8ad
Aloksingh7101 <aloksinghnitpatna@gmail.com>
2025-03-31 19:45:46 +0530

    updated

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Home from './home/Home';
-import Course from './components/Course';
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Courses from './courses/Courses';
 import Signup from './components/Signup';
 import { Toaster } from 'react-hot-toast';
@@ -9,22 +8,20 @@ import { useAuth } from './context/Authpro';
 import Contect from './components/Contect';
 
 function App() {
-  const [authUser, setAuthUser] = useAuth();
+  const [authUser] = useAuth(); // Removed unused setAuthUser
   console.log(authUser);
   
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route 
-            path="/course" 
-            element={authUser ? <Courses/> : <Navigate to="/signup"/>}
-          />
-          <Route path="/signup" element={<Signup/>}/>
-          <Route path="/contact" element={<Contect/>}/>
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route 
+          path="/course" 
+          element={authUser ? <Courses/> : <Navigate to="/signup"/>}
+        />
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/contact" element={<Contect/>}/>
+      </Routes>
       <Toaster />
     </>
   );
