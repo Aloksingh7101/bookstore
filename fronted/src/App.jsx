1a81b67694eabf02b598c202444a16f9c7834795
Aloksingh7101 <aloksinghnitpatna@gmail.com>
2025-03-31 19:40:16 +0530

    updated

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -1,36 +1,33 @@
-import React from 'react'
-import Home from './home/Home'
-import Course from './components/Course'
-import {Navigate, Route,Routes} from "react-router-dom"
-import Courses from './courses/Courses'
-import Signup from './components/Signup'
+import React from 'react';
+import Home from './home/Home';
+import Course from './components/Course';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Courses from './courses/Courses';
+import Signup from './components/Signup';
 import { Toaster } from 'react-hot-toast';
-import { useAuth } from './context/Authpro'
-import Contect from './components/Contect'
-
-
-
+import { useAuth } from './context/Authpro';
+import Contect from './components/Contect';
 
 function App() {
-  const [authUser,setAuthUser]=useAuth();
+  const [authUser, setAuthUser] = useAuth();
   console.log(authUser);
+  
   return (
     <>
-    {/* <Home/>
-    <Course/> */}
-    <Routes>
-
-      
-   <Route path="/" element={<Home/>}/>
-   <Route path="/course" element={authUser? <Courses/>:<Navigate to="/signup"/>}
-   />
-   <Route path="/signup" element={<Signup/>}/>
-   <Route path="/contact" element={   <Contect/>}/>
-
-    </Routes>
-    <Toaster />
-    
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route 
+            path="/course" 
+            element={authUser ? <Courses/> : <Navigate to="/signup"/>}
+          />
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/contact" element={<Contect/>}/>
+        </Routes>
+      </BrowserRouter>
+      <Toaster />
     </>
-  )
+  );
 }
+
 export default App;
